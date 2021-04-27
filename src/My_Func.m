186f54d5cd2f8fd6697d62193ca817734a80d3fe
speedytman <74835411+speedytman@users.noreply.github.com>
2021-04-27 16:00:49 -0400

    Update My_Func.m

--- src/My_Func.m
+++ src/My_Func.m
@@ -2,6 +2,6 @@ function outputArg1 = My_Func(filePath)
     I = imread(filePath);
     Text = ocr(I, 'Language', 'E:\Senior_Project_Matlab\CTS_ocr\tessdata\CTS_ocr.traineddata');
     
-    outputArg1 = Text;
+    outputArg1 = Text.Text;
 end
 
