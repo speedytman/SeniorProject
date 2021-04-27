ac25a248b293abb9aade620531c9ceda2d609faa
speedytman <74835411+speedytman@users.noreply.github.com>
2021-04-27 16:08:25 -0400

    Update My_Func.m
    Changed location of custom OCR and fixed mising .Text return from OCR

--- src/My_Func.m
+++ src/My_Func.m
@@ -1,7 +1,6 @@
 function outputArg1 = My_Func(filePath)
     I = imread(filePath);
-    Text = ocr(I, 'Language', 'E:\Senior_Project_Matlab\CTS_ocr\tessdata\CTS_ocr.traineddata');
+    Text = ocr(I, 'Language', '../Matlab Code/CTS_ocr/tessdata/CTS_ocr.traineddata');
     
     outputArg1 = Text.Text;
 end
-
