c5b153f4e75cda6e85fd06cda3e8c830b9bd3b0f
speedytman <74835411+speedytman@users.noreply.github.com>
2021-04-27 00:22:38 -0400

    Update Stuff.m

--- Matlab Code/Stuff.m
+++ Matlab Code/Stuff.m
@@ -1,22 +1,32 @@
+%loads data from file listed
 my_data = load('Copy_of_mydata.mat');
-
+%get table from file
 trainingData = my_data.trainingData;
 
-for x = 278:281
+%loops through all image names in trainingdata
+for x = 1:height(trainingData)
+    %reads the image
     I = imread(append('dataset/train/images/', trainingData.imageFilename{x}));
-    trainingData.imageFilename{x}
-    figure,imshow(I)
+    
+    %uncomment to show the file name of the current file
+    %trainingData.imageFilename{x}
+    
+    %uncomment bellow code to show each image
+    %figure,imshow(I)
+    
+    %some images have more than one bounding box, loops thorugh each
     for y = 1:height(trainingData.licensePlate{x})
+        %crops the image to the bounding box size
         cropped = imcrop(I, trainingData.licensePlate{x}(y,:));
-        imageFileName = sprintf('Train_OCR/croppedImage%d.jpg',x);
+        %generates file name
+        imageFileName = sprintf('Train_OCR/croppedImage%d_%d.jpg', x, y);
+        %writes new image
         imwrite(cropped, imageFileName);
-%         text{x} = ocr(cropped);
-%         if height(text{x}.CharacterBoundingBoxes) == 0
-%             lvl = graythresh(cropped);
-%             BWOrig = im2bw(cropped, lvl);
-%             text{x} = ocr(BWOrig);
-%             text{x}.Text
-%         end
+        %runs cropped image through matlabs built in OCR
+        text{x} = ocr(cropped);
+        %uncomment to display each the predicion for each image
+        %text{x}.Text
+%         %display each image after it is cropped
 %         figure, imshow(cropped)
     end
 end
